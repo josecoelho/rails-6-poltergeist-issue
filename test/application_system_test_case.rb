44bc369fa9e55a3bc7fad1af59aecc9351b782e4
José Coelho <jose.coelho001@msd.govt.nz>
2020-04-01 09:52:35 +1300

    Change to run using firefox

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,5 @@
 require "test_helper"
-require "capybara/poltergeist"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :poltergeist, screen_size: [1400, 1400], options:
-    { js_errors: false }
+  driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
 end
